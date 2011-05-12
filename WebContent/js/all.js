dd27a4801d8ad2c20791584a8f9b7aa2c881dca0
Fernando Silveira <fsilveira@gmail.com>
2011-05-12 13:44:07 -0300

    Removed buggy JavaScript codes but sadly I don't really know the impact.

--- WebContent/js/all.js
+++ WebContent/js/all.js
@@ -2,8 +2,8 @@
  * 
  */
 
-$('input:text').bind('focus blur', function() {
-    $(this).toggleClass('highlight');
-});
-
-$(':input:visible').each(i,e){ $(e).attr('tabindex, i) });
+// These four lines below were being complained by WebKit. What should they do?
+//$('input:text').bind('focus blur', function() {
+//    $(this).toggleClass('highlight');
+//});
+//$(':input:visible').each(i,e){ $(e).attr('tabindex, i) });
