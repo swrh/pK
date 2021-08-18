58964adb415ff45f317c4a7c550660f351debcac
Fernando Silveira <fsilveira@gmail.com>
2021-08-18 08:36:42 -0300

    Importing skeleton from start.spring.io

---
+++ src/main/java/com/github/swrh/pk/PKApplication.java
@@ -0,0 +1,13 @@
+package com.github.swrh.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/swrh/pk/PKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.swrh.pk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
