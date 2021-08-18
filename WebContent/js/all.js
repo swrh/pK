2e4fcac48036dc5b77c6dc2dfffa79d8b6a9baea
Fernando Silveira <fsilveira@gmail.com>
2021-08-18 09:15:29 -0300

    Resetting source code

--- WebContent/js/all.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-
-// These four lines below were being complained by WebKit. What should they do?
-//$('input:text').bind('focus blur', function() {
-//    $(this).toggleClass('highlight');
-//});
-//$(':input:visible').each(i,e){ $(e).attr('tabindex, i) });
