e550dd02b54500dc224c66eeeb9b3472ee37448d
Fernando Silveira <fsilveira@gmail.com>
2021-08-18 08:54:10 -0300

    Implemented UserRepository

---
+++ src/main/java/com/github/swrh/pk/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.github.swrh.pk.repositories;
+
+import com.github.swrh.pk.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByName(String name);
+
+}
